750cf8585f918f8db7b823e3a719d977a8aa1c0a
codewasp942 <tmz50301@163.com>
2021-01-28 13:02:48 +0800

    1

--- WallChess.cpp
+++ WallChess.cpp
@@ -1,5 +1,6 @@
 ﻿/*
-https://github.com/codewasp942/
+https://github.com/codewasp942/WallChess
+Visual Studio 2019
 */
 
 
@@ -8,15 +9,20 @@ https://github.com/codewasp942/
 #include <windows.h>
 using namespace std;
 
-bool mp[3][109][109];
-int xw = 7, yw = 7;
+bool mp[3][109][109];		//某位置是否有墙
+int xw = 7, yw = 7;			//棋盘宽度、高度
 
 int main(){
 
 	HANDLE hout=GetStdHandle(STD_OUTPUT_HANDLE);
 	if (hout == INVALID_HANDLE_VALUE) {
-		MessageBox(HWND_DESKTOP, TEXT("Error"), TEXT("The consle can be only black-white."));
+		MessageBox(HWND_DESKTOP,
+			TEXT("尴尬了，无法获取输出句柄。"),
+			TEXT("运行错误"),
+			MB_ICONWARNING | MB_OK);
+		return 5201314;
 	}
 
 	pos p1(1, 1),p2(xw, yw);
+	return 0;
 }
