0992914e9edcc9ed95ce97642a56f9c456b1582d
codewasp942 <tmz50301@163.com>
2021-01-28 16:02:37 +0800

    create loop

--- WallChess.cpp
+++ WallChess.cpp
@@ -8,12 +8,15 @@ Visual Studio 2019 on windows
 #include <windows.h>
 #include <cstdio>
 #include <cmath>
+#include <conio.h>
 #include "position.h"
 #include "outex.h"
 
 using namespace std;
 
 bool mp[3][109][109];		//某位置是否有墙
+bool cmove[109][109];		//是否可达
+bool keys[300];				//键盘状态
 int xw = 7, yw = 7;			//棋盘宽度、高度
 
 int main(){
@@ -24,6 +27,13 @@ int main(){
 	}
 
 	pos p1(1, 1),p2(xw, yw);
-	for(int i=1;i<=60;i++)outch('o',i,sin(i/5.0)*10+10,COL_BLACK,COL_BLUE);
+	char ch;
+	
+	while (1) {
+		ch = getchar();
+		printf("\033[2J");
+		cout << (int)ch;
+	}
+
 	return 0;
 }
