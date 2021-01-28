a57689f47df9fd6422adc261be66dec4856e0337
codewasp942 <tmz50301@163.com>
2021-01-28 13:59:12 +0800

    22

--- WallChess.cpp
+++ WallChess.cpp
@@ -1,6 +1,6 @@
 ﻿/*
 https://github.com/codewasp942/WallChess
-Visual Studio 2019
+Visual Studio 2019 on windows
 */
 
 
@@ -13,16 +13,9 @@ bool mp[3][109][109];		//某位置是否有墙
 int xw = 7, yw = 7;			//棋盘宽度、高度
 
 int main(){
-
-	HANDLE hout=GetStdHandle(STD_OUTPUT_HANDLE);
-	if (hout == INVALID_HANDLE_VALUE) {
-		MessageBox(HWND_DESKTOP,
-			TEXT("尴尬了，无法获取输出句柄。"),
-			TEXT("运行错误"),
-			MB_ICONWARNING | MB_OK);
-		return 5201314;
-	}
-
 	pos p1(1, 1),p2(xw, yw);
+
+	cout << "\033[31m111\033[0m";
+	
 	return 0;
 }
---
+++ outex.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void outch(char, int, int, int, int);
